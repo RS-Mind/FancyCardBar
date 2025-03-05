43ff7e6e709814e553c3a3d12d1e97070b3c4e90
RS_Mind <jared@awoodfamily.com>
2025-03-05 14:09:02 -0700

    1.3.0
    Added auto icon generator

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,3 +3,6 @@
 
 #### 1.2.0:
 - Added a config option to disable the mod, allowing mod authors to safely set this mod as a dependency
+
+#### 1.3.0
+- Added a config option to automatically generate icons for cards with art but lacking an icon. Disabled by default
--- FancyCardBar.cs
+++ FancyCardBar.cs
@@ -19,13 +19,21 @@ namespace FancyCardBar
         private const string ModId = "com.rsmind.rounds.fancycardbar";
         private const string ModName = "Fancy Card Bar";
         private const string CompatibilityModName = "FancyCardBar";
-        public const string Version = "1.2.0";
+        public const string Version = "1.3.0";
         public const string ModInitials = "FCB";
         public static FancyCardBar instance { get; private set; }
 
         void Awake()
         {
             Unbound.RegisterClientSideMod(ModId);
+
+            assets = AssetUtils.LoadAssetBundleFromResources("fancycardbar", typeof(FancyCardBar).Assembly);
+
+            if (assets == null)
+            {
+                UnityEngine.Debug.Log("Failed to load Fancy Card Bar asset bundle");
+            }
+
             var harmony = new Harmony(ModId);
             harmony.PatchAll();
         }
@@ -34,8 +42,37 @@ namespace FancyCardBar
         void Start()
         {
             instance = this;
-
+            blankIcon = assets.LoadAsset<GameObject>("I_Template");
             Unbound.RegisterMenu(ModName, () => { }, NewGUI, null, false);
+            if(genIcons) instance.ExecuteAfterFrames(20, () => GenerateIcons());
+        }
+
+        internal static void GenerateIcons()
+        {
+            foreach (CardInfo cardInfo in CardChoice.instance.cards)
+            {
+                if (cardInfo.gameObject.GetComponent<FancyIcon>() is FancyIcon fancyIcon) continue; // Skip auto-generation if there's already an icon
+                if (cardInfo.cardArt is GameObject art)
+                {
+                    FancyIcon icon = cardInfo.gameObject.AddComponent<FancyIcon>();
+                    icon.fancyIcon = Instantiate(blankIcon);
+                    icon.fancyIcon.transform.SetParent(icon.transform);
+                    GameObject artObject = Instantiate(art, icon.fancyIcon.transform);
+                    artObject.transform.SetAsFirstSibling();
+                    artObject.transform.localScale = new Vector3(0.15f, 0.15f, 0.15f);
+
+                    var animators = artObject.GetComponents<Animator>();
+                    foreach(Animator animator in animators)
+                    {
+                        Destroy(animator);
+                    }
+                    animators = artObject.GetComponentsInChildren<Animator>();
+                    foreach (Animator animator in animators)
+                    {
+                        Destroy(animator);
+                    }
+                }
+            }
         }
 
         internal static string GetConfigKey(string name)
@@ -55,14 +92,29 @@ namespace FancyCardBar
             }
         }
 
+        public static bool genIcons
+        {
+            get
+            {
+                return PlayerPrefs.GetInt(GetConfigKey("genIcons"), 0) == 1;
+            }
+            internal set
+            {
+                PlayerPrefs.SetInt(GetConfigKey("genIcons"), value ? 1 : 0);
+            }
+        }
+
         private static void NewGUI(GameObject menu)
         {
             MenuHandler.CreateText(ModName + " Options", menu, out TextMeshProUGUI _, 60);
             MenuHandler.CreateText(" ", menu, out TextMeshProUGUI _, 30);
             MenuHandler.CreateToggle(modActive, "Use Fancy Icons", menu, (bool val) => { modActive = val; });
+            MenuHandler.CreateText(" ", menu, out TextMeshProUGUI _, 30);
+            MenuHandler.CreateToggle(genIcons, "Automatically Generate Card Icons (Requires Restart)", menu, (bool val) => { genIcons = val; });
         }
 
         public static bool Debug = false;
         internal static AssetBundle assets;
+        internal static GameObject blankIcon;
     }
 }
